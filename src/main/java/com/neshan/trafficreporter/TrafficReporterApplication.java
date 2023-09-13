226b4c9053b96d02bce2e461e62df253d665b894
Ali Shahidi <alishahidi1376@gmail.com>
2023-09-13 19:32:42 +0330

    init commit

---
+++ src/main/java/com/neshan/trafficreporter/TrafficReporterApplication.java
@@ -0,0 +1,13 @@
+package com.neshan.trafficreporter;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TrafficReporterApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TrafficReporterApplication.class, args);
+	}
+
+}
