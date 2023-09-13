b21954087edaae2ff8eca72eb39353883259c9b8
Ali Shahidi <alishahidi1376@gmail.com>
2023-09-13 19:56:09 +0330

    add User Repository

---
+++ src/main/java/com/neshan/trafficreporter/repository/UserRepository.java
@@ -0,0 +1,12 @@
+package com.neshan.trafficreporter.repository;
+
+import com.neshan.trafficreporter.model.User;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+import java.util.Optional;
+
+@Repository
+public interface UserRepository extends JpaRepository<User, Long> {
+    Optional<User> findByUsername(String username);
+}
