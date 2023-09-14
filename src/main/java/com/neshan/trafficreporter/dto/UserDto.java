1d4bd049269501db9bc224327ab5976f29d2dbcd
Ali Shahidi <alishahidi1376@gmail.com>
2023-09-14 10:27:13 +0330

    add User dto

--- src/main/java/com/neshan/trafficreporter/dto/UserDto.java
+++ src/main/java/com/neshan/trafficreporter/dto/UserDto.java
@@ -1,7 +1,17 @@
 package com.neshan.trafficreporter.dto;
 
+import com.fasterxml.jackson.annotation.JsonIgnore;
+import com.neshan.trafficreporter.enums.Role;
+import com.neshan.trafficreporter.model.Report;
+import com.neshan.trafficreporter.model.Route;
+import jakarta.persistence.*;
 import lombok.*;
 import lombok.experimental.FieldDefaults;
+import org.hibernate.annotations.CreationTimestamp;
+import org.hibernate.annotations.UpdateTimestamp;
+
+import java.util.Date;
+import java.util.List;
 
 @Builder
 @AllArgsConstructor
@@ -13,5 +23,10 @@ public class UserDto {
     Long id;
     String name;
     String username;
+    Role role;
+    List<ReportDto> reports;
+    List<RouteDto> routes;
+    Date createdAt;
+    Date updatedAt;
 
 }
