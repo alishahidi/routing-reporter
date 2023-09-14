4bb98bf91a55cc57c0818f900594adffa965b663
Ali Shahidi <alishahidi1376@gmail.com>
2023-09-14 12:08:12 +0330

    fix bad patch problem

---
+++ src/main/java/com/neshan/trafficreporter/RoutingReporterApplication.java
@@ -0,0 +1,13 @@
+package com.neshan.trafficreporter;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RoutingReporterApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(RoutingReporterApplication.class, args);
+    }
+
+}
