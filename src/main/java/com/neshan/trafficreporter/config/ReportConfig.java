51c64a4c1cf7364da2ff34c5fb40ecf42f2c8227
Ali Shahidi <alishahidi1376@gmail.com>
2023-09-15 11:33:28 +0330

    add police and accident report configs

--- src/main/java/com/neshan/trafficreporter/config/ReportConfig.java
+++ src/main/java/com/neshan/trafficreporter/config/ReportConfig.java
@@ -9,11 +9,22 @@ import org.springframework.context.annotation.Configuration;
 public class ReportConfig {
     @Value("${report.traffic.init.ttl}")
     private Integer initTrafficTtl;
-
     @Value("${report.traffic.like.ttl}")
     private Integer likeTrafficTtl;
-
     @Value("${report.traffic.dis-like.ttl}")
     private Integer disLikeTrafficTtl;
 
+    @Value("${report.police.init.ttl}")
+    private Integer initPoliceTtl;
+    @Value("${report.police.like.ttl}")
+    private Integer likePoliceTtl;
+    @Value("${report.police.dis-like.ttl}")
+    private Integer disLikePoliceTtl;
+
+    @Value("${report.accident.init.ttl}")
+    private Integer initAccidentTtl;
+    @Value("${report.accident.like.ttl}")
+    private Integer likeAccidentTtl;
+    @Value("${report.accident.dis-like.ttl}")
+    private Integer disLikeAccidentTtl;
 }
