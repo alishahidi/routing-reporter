885e2310b5f6520cf92f48b87bc1cb8e556679c1
Ali Shahidi <alishahidi1376@gmail.com>
2023-09-15 23:40:18 +0330

    dockerize application with envs

--- src/main/java/com/neshan/routingreporter/config/RedisConfig.java
+++ src/main/java/com/neshan/routingreporter/config/RedisConfig.java
@@ -3,17 +3,23 @@ package com.neshan.routingreporter.config;
 import org.redisson.Redisson;
 import org.redisson.api.RedissonClient;
 import org.redisson.config.Config;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 
 @Configuration
 public class RedisConfig {
+    @Value("${spring.data.redis.host}")
+    private String redisHost;
+
+    @Value("${spring.data.redis.port}")
+    private Integer redisPort;
 
     @Bean
     public RedissonClient redissonClient() {
         Config config = new Config();
         config.useSingleServer()
-                .setAddress("redis://localhost:6379");
+                .setAddress("redis://" + redisHost + ":" + redisPort);
 
         return Redisson.create(config);
     }
